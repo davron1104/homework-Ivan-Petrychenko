b84d4bb576d1a5598eeef67629fc928615da1edd
Davron <davron1104@yandex.com>
2020-12-21 22:22:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+' use strict ';
+const numberOfFilms = prompt(" Сколько фильмов вы уже посмотрели? ", "");
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?' , ""),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('Один из последних просмотренных фильмов?', '');
+
+      personalMovieDB.movies[a]=b;
+      personalMovieDB.movies[c]=d;
+      console.log(personalMovieDB);
