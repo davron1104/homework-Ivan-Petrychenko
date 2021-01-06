ad97d7f874880badf36bd469c3a12b34a7a6f687
Davron <davron1104@yandex.com>
2021-01-06 22:54:37 +0500

    third commit

--- script.js
+++ script.js
@@ -22,15 +22,15 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
 
 // let a = prompt ('Один из последних просмотренных фильмов?', ''),
 //     b = prompt ('На сколько оцените его?', ''),
@@ -107,7 +107,7 @@ const personalMovieDB = {
 // }
 // console.log(personalMovieDB);
 
-let i = 0;
+// let i = 0;
 // while (i < 2) {
 //     i++;
 //     const a = prompt('Один из последних просмотренных фильмов?', ''),
@@ -117,7 +117,7 @@ let i = 0;
 //     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
 //                 personalMovieDB.movies[a] = b;
 //                 console.log('done');
-        
+
 //             } else {
 //                 console.log('error');
 
@@ -135,7 +135,7 @@ let i = 0;
 //     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
 //                 personalMovieDB.movies[a] = b;
 //                 console.log('done');
-        
+
 //             } else {
 //                 console.log('error');
 //                 i--;
@@ -144,6 +144,57 @@ let i = 0;
 // while (i < 2); 
 
 
+// 18. Практика , ч3. Используем функции:
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+        personalMovieDB.movies[a] = b;
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+    
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log("Вы классический зритель");
+    
+    } else if (personalMovieDB.count > 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+    
+}
+detectPersonalLevel();
 
 
 
@@ -151,7 +202,23 @@ let i = 0;
 
 
 
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
 
+function writeYourGenres() {
+ for (let i = 1; i <= 3; i++) {
+     const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+     personalMovieDB.genres[i - 1] = genre; 
+ }        
+}
+
+writeYourGenres();
+
+// console.log(personalMovieDB);
 
 
 
